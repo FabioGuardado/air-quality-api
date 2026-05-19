ef99d87f45c7e108442327afc7dda45abf867096
Fabio Guardado <fabio.guardado2012@gmail.com>
2026-05-18 20:07:09 -0600

    Agregar coordenadas para consultar api clima

--- Sensores.Domain/Entities/SensorCalidadAire.cs
+++ Sensores.Domain/Entities/SensorCalidadAire.cs
@@ -8,7 +8,8 @@ namespace Sensores.Domain.Entities
     {
         public string Ubicacion { get; set; }
         public string Estado { get; set; }
-
+        public float Lat { get; set; }
+        public float Lon { get; set; }
         public List<LecturaAire> Lecturas { get; set; }
         public List<AlertaAire> Alertas { get; set; }
     }

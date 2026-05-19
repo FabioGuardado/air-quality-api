586d2b11d46de0c7e4e4cac0344ad7f8e79f390d
Fabio Guardado <fabio.guardado2012@gmail.com>
2026-05-18 19:34:30 -0600

    Add project files.

---
+++ Sensores.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sensores.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
